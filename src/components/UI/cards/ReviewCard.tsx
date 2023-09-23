258b41c9eb5efd73807eaa3ab44754f51c708644
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-23 16:07:48 +0300

    done

--- src/components/UI/cards/ReviewCard.tsx
+++ src/components/UI/cards/ReviewCard.tsx
@@ -17,7 +17,7 @@ interface ReviewProps {
 
 
 export default component$<ReviewProps>(({review}) => {
-    const {width} = useWindowSize();
+    const width = useWindowSize();
     return (
         <div class={'p-6 xl:p-10 flex flex-col gap-6 bg-white rounded-[30px] xl:min-w-[805px] xl:flex-row xl:gap-12'}>
             <div class={'flex flex-col gap-1'}>
