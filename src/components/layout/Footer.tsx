355a3972e3f3e5123026da0c830f0d0c48222625
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-25 17:58:31 +0300

    done

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,6 @@
-import {component$} from "@builder.io/qwik";
+import {component$, $} from "@builder.io/qwik";
 import FooterEmailInput from "~/components/UI/input/text/FooterEmailInput";
+import UpButton from "../../assets/icons/UpButton.svg?jsx"
 import {Link} from "@builder.io/qwik-city";
 import ContactSocials from "~/components/sections/contact-page/ContactSocials";
 
@@ -20,11 +21,25 @@ export default component$(() => {
             ],
 };
 
+        const smoothScrollToTop = $(() => {
+            const step = document.documentElement.scrollHeight / 100;
+            let currentPosition = window.scrollY || window.pageYOffset;
+        
+            const scrollStep = () => {
+                if (currentPosition > 0) { 
+                    currentPosition -= step;
+                    window.scrollTo(0, currentPosition);
+                    requestAnimationFrame(scrollStep);
+                }
+            }
+        
+            requestAnimationFrame(scrollStep);
+        });
 
 
     return (
         <footer class="px-2 lg:px-7 xl:px-10 pb-4 bg-bg">
-            <div class={'py-6 lg:py-8 xl:py-16 px-4 lg:px-6 xl:px-12 flex flex-col gap-11 bg-main rounded-3xl'}>
+            <div class={'relative py-6 lg:py-8 xl:py-16 px-4 lg:px-6 xl:px-12 flex flex-col gap-11 bg-main rounded-3xl'}>
                 <div class={'flex flex-col gap-3'}>
                 <span class={'text-lg xl:text-[40px] leading-5 xl:leading-[44px] text-white'}>
                   Let’s stay in touch.
@@ -33,12 +48,12 @@ export default component$(() => {
                 </div>
                 <div class={'flex flex-col gap-11 lg:flex-row lg:justify-between'}>
                     <div class={'flex flex-col gap-3'}>
-                    <span class={'text-lg leading-5 text-white'}>
+                    <span class={'text-lg leading-5 text-white xl:mt-28'}>
                       Följ oss
                     </span>
                         <ContactSocials mode={"light"}/>
                     </div>
-                    <div class={'flex gap-[56px] '}>
+                    <div class={'flex gap-[56px] xl:-mt-[140px]'}>
                         <div class={'flex flex-col gap-4'}>
                     <span class={'text-lightGrey'}>
                         Categories
@@ -63,17 +78,28 @@ export default component$(() => {
                                 )}
                             </div>
                         </div>
+                        <div
+                            class="flex flex-col justify-end items-end -ml-10 xl:-mb-[40px]"
+                            >
+                            <div>
+                                <UpButton onClick$={smoothScrollToTop}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div class={'flex justify-center gap-[50px]'}>
-                    <span class={'text-sm text-accent3'}>Terms of Use</span>
-                    <span class={'text-sm text-accent3'}>Privacy Policy</span>
+                <div class="flex xs:flex-col xs:gap-[28px] mt-[32px] xl:-mb-[27px]">
+                    <div class={'xl:absolute bottom-[36px] left-[40%] flex justify-center gap-[50px]'}>
+                        <span class={'text-sm text-accent3'}>Terms of Use</span>
+                        <span class={'text-sm text-accent3'}>Privacy Policy</span>
+                    </div>
+                    <div
+                    class="flex xs:gap-[28px] xs:flex-col lg:flex-row-reverse lg:justify-between"
+                    >
+                        <span class={'text-sm text-accent3 text-center'}>Designed by Kolodiazhenskyi</span>
+                        <span class={'text-sm text-accent3 text-center'}>© 2023 | LoveSpace | All Rights Reserved</span>
+                    </div>
                 </div>
-                <span class={'text-sm text-accent3 text-center'}>Designed by Kolodiazhenskyi</span>
-                <span class={'text-sm text-accent3 text-center'}>© 2023 | LoveSpace | All Rights Reserved</span>
             </div>
-
         </footer>
     );
 });
